656151b9d770d62fce7a13ad03101b5508a66d1d
andre.borba <dev_andre.filipe@outlook.com>
2022-08-18 02:54:09 -0300

    leap_year

--- leap_year.rb
+++ leap_year.rb
@@ -9,7 +9,7 @@ end
 describe 'leap_year?' do
   context 'when given two years' do
     it 'should return leap years between the two years' do
-      expect(leap_years(2000, 2016)).to eq([2000, 2004, 2008, 2012, 2016])
+      expect(leap_years(2000, 2022)).to eq([2000, 2004, 2008, 2012, 2016, 2020])
     end
   end
 end
